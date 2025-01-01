6c1b02abe56df1f4af90a441958d82303d7e9c58
pastchum <patrick.navin.thomas123@gmail.com>
2025-01-01 23:13:31 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
+"use client";
+
+import { useState } from "react";
+
+import { Input } from "@nextui-org/input";
 import Image from "next/image";
 
+import eyeIcon from "@/icons/eye-open.png";
+import eyeClosedIcon from "@/icons/eye-invisible.png";
+
 export default function Home() {
+  const [userID, setUserID] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const handleSubmit = () => {};
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="flex flex-row justify-between w-full">
+        <div>test</div>
+        <div className="w-1/5 space-y-2 mt-32">
+          <div className="text-2xl font-bold py-2">Client Login</div>
+          Enter your Northern Trust credentials below to sign in. Please contact
+          your relationship manager if you need to register.
+          <Input
+            label="UserID"
+            type="UserID"
+            onChange={(e) => setUserID(e.target.value)}
+          />
+          <Input
+            label="Password"
+            type={visible ? "text" : "password"}
+            onChange={(e) => setPassword(e.target.value)}
+            endContent={
+              <button onClick={() => setVisible(!visible)}>
+                {visible ? (
+                  <Image
+                    src={eyeClosedIcon}
+                    className="w-5 h-5"
+                    alt="Invisible"
+                  ></Image>
+                ) : (
+                  <Image
+                    src={eyeIcon}
+                    className="w-5 h-5"
+                    alt="Visible"
+                  ></Image>
+                )}
+              </button>
+            }
+          />
+          <div className="py-3 flex justify-between">
+            <button type="checkbox"></button>
+            <button className="underline text-green-700 text-xs">
+              Forgot Password?
+            </button>
+          </div>
+          <button onClick={handleSubmit} className="w-full py-3">
+            <div className="w-8/10 flex p-2 border bg-green-700 text-slate-50 flex align-center justify-center">
+              Log In
+            </div>
+          </button>
+          <div className="justify-center p-10 text-green-700 underline">
+            <a href="https://www.northerntrust.com/united-states/contact-us-corporate-overview">
+              Passport Help
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
